8222f0642475850ef45290f69377b3bb4ec7159b
Diana Bejan <diana.bejan@brainloop.com>
2019-06-30 13:39:40 +0300

    Add tests

--- pkg/server/server_test.go
+++ pkg/server/server_test.go
@@ -1,1 +1,200 @@
-package server_test
+package server
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"github.com/bitstored/crypto-service/pb"
+	"github.com/bitstored/crypto-service/pkg/service"
+)
+
+func TestNewCryptoServer(t *testing.T) {
+	type args struct {
+		s *service.CryptoService
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Server
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewCryptoServer(tt.args.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewCryptoServer() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestServer_EncryptFile(t *testing.T) {
+	type fields struct {
+		cryptoService *service.CryptoService
+	}
+	type args struct {
+		ctx context.Context
+		in  *pb.EncryptFileRequest
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *pb.EncryptFileResponse
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Server{
+				cryptoService: tt.fields.cryptoService,
+			}
+			got, err := s.EncryptFile(tt.args.ctx, tt.args.in)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Server.EncryptFile() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Server.EncryptFile() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestServer_DecryptFile(t *testing.T) {
+	type fields struct {
+		cryptoService *service.CryptoService
+	}
+	type args struct {
+		ctx context.Context
+		in  *pb.DecryptFileRequest
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *pb.DecryptFileResponse
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Server{
+				cryptoService: tt.fields.cryptoService,
+			}
+			got, err := s.DecryptFile(tt.args.ctx, tt.args.in)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Server.DecryptFile() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Server.DecryptFile() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestServer_EncryptPassword(t *testing.T) {
+	type fields struct {
+		cryptoService *service.CryptoService
+	}
+	type args struct {
+		ctx context.Context
+		in  *pb.EncryptPasswordRequest
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *pb.EncryptPasswordResponse
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Server{
+				cryptoService: tt.fields.cryptoService,
+			}
+			got, err := s.EncryptPassword(tt.args.ctx, tt.args.in)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Server.EncryptPassword() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Server.EncryptPassword() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestServer_Encrypt(t *testing.T) {
+	type fields struct {
+		cryptoService *service.CryptoService
+	}
+	type args struct {
+		ctx context.Context
+		in  *pb.EncryptRequest
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *pb.EncryptResponse
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Server{
+				cryptoService: tt.fields.cryptoService,
+			}
+			got, err := s.Encrypt(tt.args.ctx, tt.args.in)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Server.Encrypt() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Server.Encrypt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestServer_Decrypt(t *testing.T) {
+	type fields struct {
+		cryptoService *service.CryptoService
+	}
+	type args struct {
+		ctx context.Context
+		in  *pb.DecryptRequest
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *pb.DecryptResponse
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Server{
+				cryptoService: tt.fields.cryptoService,
+			}
+			got, err := s.Decrypt(tt.args.ctx, tt.args.in)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Server.Decrypt() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Server.Decrypt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
--- pkg/service/service_test.go
+++ pkg/service/service_test.go
@@ -1,1 +1,174 @@
 package service
+
+import (
+	"context"
+	"reflect"
+	"testing"
+)
+
+func TestCryptoService_EncryptFile(t *testing.T) {
+	type args struct {
+		ctx          context.Context
+		content      []byte
+		secretPhrase []byte
+	}
+	tests := []struct {
+		name    string
+		cs      *CryptoService
+		args    args
+		want    []byte
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cs := &CryptoService{}
+			got, err := cs.EncryptFile(tt.args.ctx, tt.args.content, tt.args.secretPhrase)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CryptoService.EncryptFile() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CryptoService.EncryptFile() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCryptoService_DecryptFile(t *testing.T) {
+	type args struct {
+		ctx          context.Context
+		content      []byte
+		secretPhrase []byte
+	}
+	tests := []struct {
+		name    string
+		cs      *CryptoService
+		args    args
+		want    []byte
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cs := &CryptoService{}
+			got, err := cs.DecryptFile(tt.args.ctx, tt.args.content, tt.args.secretPhrase)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CryptoService.DecryptFile() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CryptoService.DecryptFile() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCryptoService_EncryptPassword(t *testing.T) {
+	type args struct {
+		ctx      context.Context
+		password []byte
+		salt     []byte
+		iter     int
+	}
+	tests := []struct {
+		name     string
+		cs       *CryptoService
+		args     args
+		wantHash []byte
+		wantErr  bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cs := &CryptoService{}
+			gotHash, err := cs.EncryptPassword(tt.args.ctx, tt.args.password, tt.args.salt, tt.args.iter)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CryptoService.EncryptPassword() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotHash, tt.wantHash) {
+				t.Errorf("CryptoService.EncryptPassword() = %v, want %v", gotHash, tt.wantHash)
+			}
+		})
+	}
+}
+
+func TestCryptoService_Encrypt(t *testing.T) {
+	type args struct {
+		ctx  context.Context
+		data []byte
+		salt []byte
+	}
+	tests := []struct {
+		name    string
+		cs      *CryptoService
+		args    args
+		want    []byte
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cs := &CryptoService{}
+			got, err := cs.Encrypt(tt.args.ctx, tt.args.data, tt.args.salt)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CryptoService.Encrypt() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CryptoService.Encrypt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCryptoService_Decrypt(t *testing.T) {
+	type args struct {
+		ctx  context.Context
+		data []byte
+		salt []byte
+	}
+	tests := []struct {
+		name    string
+		cs      *CryptoService
+		args    args
+		want    []byte
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cs := &CryptoService{}
+			got, err := cs.Decrypt(tt.args.ctx, tt.args.data, tt.args.salt)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CryptoService.Decrypt() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CryptoService.Decrypt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewCryptoService(t *testing.T) {
+	tests := []struct {
+		name string
+		want *CryptoService
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewCryptoService(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewCryptoService() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
